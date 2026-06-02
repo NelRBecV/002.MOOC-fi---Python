341f185366b1d56ba959c0d6144a0c7777075960
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-02 11:17:33 -0300

    Create part02-19_pin_and_number_of_attempts.py

---
+++ Part2/part02-19_pin_and_number_of_attempts.py
@@ -0,0 +1,13 @@
+count=0
+	while True:
+	    count +=1
+	    pin = int(input("PIN:"))    
+	    if pin != 4321:
+	        print("Wrong")        
+	    else:
+	        if count == 1:
+	            print("Correct! It only took you one single attempt!")
+	        else:
+	            print(f"Correct! It took you {count} attempts")
+	        break
+        
