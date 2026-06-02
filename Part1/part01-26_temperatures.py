1dfe29fb5f0e27f203b7a0e4276a4d0e24eafe34
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-01 21:41:56 -0300

    Create part01-26_temperatures.py

---
+++ Part1/part01-26_temperatures.py
@@ -0,0 +1,6 @@
+	# Write your solution here
+	temp_f = int(input("Please type in a temperature (F):"))
+	celsius = ((temp_f - 32)*5)/9
+	print(f"{temp_f} degrees Fahrenheit equals {celsius} degrees Celsius")
+	if celsius < 0:
+	    print("Brr! It's cold in here!")
