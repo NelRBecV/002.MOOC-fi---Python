5c1df71d21a5f2d592f6dcdd5b70b77311d259a9
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-03 22:02:39 -0300

    Create part03-25_factorial.py

---
+++ Part3/part03-25_factorial.py
@@ -0,0 +1,13 @@
+while True:
+	    number = int(input("Please type in a number:"))
+	    count = 1
+	    fact=1
+	    if number > 0:
+	        while count <= number:
+	            fact *= count
+	            count +=1
+	    else:
+	        print("Thanks and bye!")
+	        break
+	    print(f"The factorial of the number {number} is {fact}")
+	    
