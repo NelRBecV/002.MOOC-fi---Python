ca4296a77cfd64094bdd24522466ffd310697730
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-03 21:45:50 -0300

    Create part03-13_rectangle_of_hashes.py

---
+++ Part3/part03-13_rectangle_of_hashes.py
@@ -0,0 +1,8 @@
+width = int(input("Width"))
+	height = int(input("height"))
+	if width !=0 and height != 0:
+	    row = 0
+	    while row < height:
+	        print("#"*width)
+	        row +=1
+        
