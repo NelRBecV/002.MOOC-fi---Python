dac49d474080b2819a4fcfee4a767302b47dc726
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-03 21:35:22 -0300

    Create part03-05_power_of_base_n.py

---
+++ Part3/part03-05_power_of_base_n.py
@@ -0,0 +1,9 @@
+limit = int(input("Upper limit:"))
+	base = int(input("Base:"))
+	power=0
+	count = 0
+	while base**count <= limit:
+	    power = base**count
+	    print(power)
+	    count +=1
+    
