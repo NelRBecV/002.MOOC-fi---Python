4ee3aa201c36b998ea6e8a739ef44febaab7f585
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-10 09:51:47 -0300

    Create part04-03_box_of_hashes.py

---
+++ Part4/part04-03_box_of_hashes.py
@@ -0,0 +1,15 @@
+# Copy here code of line function from previous exercise
+	def line(times:str,char:str):
+	    if char == "":
+	        char="*"
+	    print(char[0]*times)
+	 
+	def box_of_hashes(height):
+	    # You should call function line here with proper parameters
+	    for l in range(height):
+	        line(10, "#")
+	 
+	# You can test your function by calling it within the following block
+	if __name__ == "__main__":
+	    box_of_hashes(8)
+	 
