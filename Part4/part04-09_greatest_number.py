31e0469aa43d500b3c11dd8d54b6eab3cce58376
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-10 10:02:43 -0300

    Create part04-09_greatest_number.py

---
+++ Part4/part04-09_greatest_number.py
@@ -0,0 +1,13 @@
+# Write your solution here
+	def greatest_number(*value):
+	    greater = 0
+	    for i in value:        
+	        if i > greater:
+	            greater = i
+	    return greater
+    
+	# You can test your function by calling it within the following block
+	if __name__ == "__main__":
+	    greatest = greatest_number(15, 24, -88)
+	    print(greatest)
+
