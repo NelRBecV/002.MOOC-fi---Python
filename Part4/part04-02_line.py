96f7def68cb7ae0969cc4dc9f80e4733fefd1580
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-10 09:59:34 -0300

    Update part04-02_line.py

--- Part4/part04-02_line.py
+++ Part4/part04-02_line.py
@@ -3,6 +3,7 @@
 	    if char == "":
 	        char="*"
 	    print(char[0]*times)
+		
 	# You can test your function by calling it within the following block
 	if __name__ == "__main__":
 	    line(10, "")
