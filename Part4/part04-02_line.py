884518d232d0cc5531874ec4c6faf9390f71725d
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-10 09:50:39 -0300

    Create part04-02_line.py

---
+++ Part4/part04-02_line.py
@@ -0,0 +1,9 @@
+# Write your solution here
+	def line(times:int,char:str):
+	    if char == "":
+	        char="*"
+	    print(char[0]*times)
+	# You can test your function by calling it within the following block
+	if __name__ == "__main__":
+	    line(10, "")
+    
