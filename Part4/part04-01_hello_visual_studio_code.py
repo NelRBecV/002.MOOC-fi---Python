eb061975db353cbdeff6737d142e886c05a74ee7
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-10 09:49:01 -0300

    Create part04-01_hello_visual_studio_code.py

---
+++ Part4/part04-01_hello_visual_studio_code.py
@@ -0,0 +1,10 @@
+while True:
+	    text = input("Editor:").title()    
+	    if text == "Emacs" or text=="Vim" or text=="Atom":
+	        print("not good")
+	    elif text=="Word" or text=="Notepad":
+	        print("awful")
+	    elif text=="Visual Studio Code":
+	        print("an excellent choice!")
+	        break
+	 
