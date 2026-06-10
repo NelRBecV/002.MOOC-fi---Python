a2d3e7286ac5f173bf7472aca0eeb2f008404fe9
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-10 09:57:38 -0300

    Create part04-06_triangle.py

---
+++ Part4/part04-06_triangle.py
@@ -0,0 +1,15 @@
+# Copy here code of line function from previous exercise
+	def line(times:int,char):
+	    if char =="":
+	        char="*"
+	    print(char[0]*times)
+	 
+	def triangle(size):
+	    # You should call function line here with proper parameters
+	    for l in range(size):
+	        line(l+1, "#")
+	 
+	# You can test your function by calling it within the following block
+	if __name__ == "__main__":
+	    triangle(5)
+	 
