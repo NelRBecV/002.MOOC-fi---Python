1f05ba9b3ac5f1edd5ec520fc66c797dff3482b5
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-11 21:16:38 -0300

    Create part05-09_remove_smallest.py

---
+++ Part5/part05-09_remove_smallest.py
@@ -0,0 +1,18 @@
+# Write your solution here
+	def remove_smallest(numbers:list):
+	    if len(numbers)>0:
+	        new_list:list =[]        
+	        small:int = min(numbers)
+        
+	        for n in numbers:
+	            if n != small:                
+	                new_list.append(n)
+	        numbers[:] = new_list
+	 
+	if __name__=="__main__":
+	    raw_list:list = [1,2,3,5,6]    
+	    print(raw_list)    
+	    remove_smallest(raw_list)
+	    print(raw_list)
+	            
+
