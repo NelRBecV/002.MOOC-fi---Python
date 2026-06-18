3c293b94e0036c8dceb701030544c795d3dbc10b
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-18 10:09:00 -0300

    Update part06-01_largest_number.py

--- Part6/part06-01_largest_number/part06-01_largest_number.py
+++ Part6/part06-01_largest_number/part06-01_largest_number.py
@@ -1,1 +1,15 @@
-
+# write your solution here
+	def largest(file:str="numbers.txt"):
+	    larger:int=0
+	    
+      with open(file,'r') as data:
+	        for number in data: #directly read lines without using readlines() IO function
+	            num = int(number.replace("\n",""))
+	            if num>=larger:
+	                larger=num
+                
+	        return larger
+	 
+	if __name__=="__main__":
+	    large = largest()
+	    print(large)
