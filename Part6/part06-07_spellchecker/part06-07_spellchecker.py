d14283cbe5ca7ce213b2fa00d3d64771f0951d8c
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-18 11:30:53 -0300

    Update part06-07_spellchecker.py

--- Part6/part06-07_spellchecker/part06-07_spellchecker.py
+++ Part6/part06-07_spellchecker/part06-07_spellchecker.py
@@ -1,1 +1,13 @@
+some_text: str = input("Write text: ")
+dictionary: list = []
 
+with open("wordlist.txt") as word_list:
+    for word in word_list:
+        dictionary.append(word.strip())
+
+text = some_text.split(" ")
+for word in text:
+    if word.lower() not in dictionary:
+        some_text = some_text.replace(f"{word}", f"*{word}*")
+
+print(some_text)
