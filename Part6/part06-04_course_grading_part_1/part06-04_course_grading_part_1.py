96ad93046897e1e3d766a2c844be654d8700c7fe
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-18 10:33:07 -0300

    Update part06-04_course_grading_part_1.py

--- Part6/part06-04_course_grading_part_1/part06-04_course_grading_part_1.py
+++ Part6/part06-04_course_grading_part_1/part06-04_course_grading_part_1.py
@@ -1,1 +1,30 @@
+# write your solution here
+	student_file:str = input("Student file: ")
+	exercise_file:str = input("Exercise file: ")
 
+	#Get students personal data
+	students:dict = {}
+	with open(student_file) as data:    
+	    for student in data:
+	        stdt_records = student.split(";")
+	        if stdt_records[0] == "id":
+	            continue
+	        students.update({stdt_records[0]: stdt_records[1].strip() + " " + stdt_records[2].strip()})
+
+	#Get students percentage of excercises completed
+	exercises:dict = {}
+	with open(exercise_file) as data:    
+	    for student in data:
+	        xcs_records = student.split(";")
+	        if xcs_records[0] == "id":
+	            continue
+	        grades:list = []
+	        for g in range(1,len(xcs_records)):
+	            grades.append(int(xcs_records[g]))
+	        exercises.update({xcs_records[0]:grades})    
+
+	#Print excercises grade for each student
+	for id, name in students.items():    
+	    if id in exercises:
+	        print(f"{name} {sum(exercises[id])}")
+        
