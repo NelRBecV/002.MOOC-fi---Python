f04de9f3ca7ff8a264bb0d19c167cc6075c1be0c
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-19 21:42:46 -0300

    Update part06-11_diary.py

--- Part6/part06-11_diary/part06-11_diary.py
+++ Part6/part06-11_diary/part06-11_diary.py
@@ -1,1 +1,18 @@
-
+op:str = ""
+	while op != "0":
+	    print("1 - add an entry, 2 - read entries, 0 - quit")
+	    op = input("Function: ")
+	    
+      if op == "1":
+	        entry = input("Diary entry: ")
+	        with open("diary.txt","a") as file:
+	            file.write(f"{entry}\n")
+	            print("Diary saved")
+	    elif op == "2":
+	        with open("diary.txt") as data:
+	            print("Entries: ")
+	            for entry in data:
+	                print(entry.strip())
+	    else:
+	        print("Bye now!")
+	         
