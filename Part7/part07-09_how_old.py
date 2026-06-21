b99b02e45b347c34a9a89d1789d37e4b09188db7
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-20 23:40:54 -0300

    Create part07-09_how_old.py

---
+++ Part7/part07-09_how_old.py
@@ -0,0 +1,26 @@
+	# Write your solution here
+from datetime import date
+
+millenium_date = date(year=1999,month=12,day=31)
+
+#checks if the introduced date is valid
+try:
+    day = int(input("Day: "))
+    month = int(input("Month: "))
+    year = int(input("Year: "))
+    birth_date= date(year=year, month=month, day=day)
+    error = False  
+except ValueError:
+        print(f"ERROR: {day}-{month}-{year} is not a valid date")
+        error = True
+
+#if date is correct the program calculates how namy days there is since your born and millenium eve
+if not error:
+    days_since_millenium = millenium_date - birth_date
+
+    #if days is greater than 0, it show a message telling how many days you had on millenium eve
+    if days_since_millenium.days > 0:
+        print(f"You were {days_since_millenium.days} days old on the eve of the new millennium.")
+    else:
+        print("You weren't born yet on the eve of the new millennium.")
+ 
