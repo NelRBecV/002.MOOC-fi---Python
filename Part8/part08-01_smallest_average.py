5680dab96506acc1e77e9ba02ff3f369fc88b655
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-22 22:12:29 -0300

    Update part08-01_smallest_average.py

--- Part8/part08-01_smallest_average.py
+++ Part8/part08-01_smallest_average.py
@@ -1,18 +1,20 @@
 # Write your solution here
 	def smallest_average(person1:dict,person2:dict,person3:dict):
+		"""Returns the smallest grades average among three persons"""
+		
 	    p1: list = [i for i in person1.values()]
-      p2: list = [i for i in person2.values()]
-      p3: list = [i for i in person3.values()]    
-	    smallest:dict = {}
-
-	    if sum(p1[1:]) < sum(p2[1:]) and sum(p1[1:]) < sum(p3[1:]):	        
-	        smallest = person1
-	    elif sum(p2[1:]) < sum(p1[1:]) and sum(p2[1:]) < sum(p3[1:]):	        
-	        smallest = person2
-	    elif sum(p3[1:]) < sum(p1[1:]) and sum(p3[1:]) < sum(p2[1:]):	        
-	        smallest = person3
-	    
-	    return smallest
+		p2: list = [i for i in person2.values()]
+		p3: list = [i for i in person3.values()]    
+		smallest:dict = {}
+		
+		if sum(p1[1:]) < sum(p2[1:]) and sum(p1[1:]) < sum(p3[1:]):	        
+			smallest = person1
+		elif sum(p2[1:]) < sum(p1[1:]) and sum(p2[1:]) < sum(p3[1:]):	        
+			smallest = person2
+		elif sum(p3[1:]) < sum(p1[1:]) and sum(p3[1:]) < sum(p2[1:]):	        
+			smallest = person3
+		
+		return smallest
 	 
 	if __name__=="__main__":
 	    person1 = {'name':'pin','result1':4,'result2':1,'result3':0}
