b43205babc23aa5d05ea0647f1fbcc1828cf2bd9
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-22 22:09:46 -0300

    Create part08-02_row_sums.py

---
+++ Part8/part08-02_row_sums.py
@@ -0,0 +1,10 @@
+	# Write your solution here
+	def row_sums(matrix:list):
+	    for l in range(len(matrix)):
+	        sums = sum(matrix[l])
+	        matrix[l].append(sums)
+	 
+	if __name__=="__main__":
+	    my_matrix = [[15,18,25,2,14,6],[3,4,9,7,250,46,2],[8,6,2,1,3],[3,5]]
+	    row_sums(my_matrix)
+	    print(my_matrix)
