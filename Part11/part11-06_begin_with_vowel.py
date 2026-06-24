8ed6f8a7df631b14f411c794c8de0e9469df71fe
Nelson R.Becerra V. <nelrbecv16@gmail.com>
2026-06-24 00:32:39 -0300

    Create part11-06_begin_with_vowel.py

---
+++ Part11/part11-06_begin_with_vowel.py
@@ -0,0 +1,12 @@
+# WRITE YOUR SOLUTION HERE:
+def begin_with_vowel(words:list):
+    return [n for n in words if n[0].lower() in "aeiou"]
+ 
+ 
+if __name__ == "__main__":
+    word_list = ["automobile", "motorbike",
+                "Animal", "cat", "Dog", "APPLE", "orange"]
+  
+    for vowelled in begin_with_vowel(word_list):
+        print(vowelled)
+ 
