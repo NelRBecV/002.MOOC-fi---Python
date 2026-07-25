54936a0ae3c3a53b3b3dedaedb8025ae82253863
Nelson Becerra <relrbecv16@gmail.com>
2026-07-24 21:06:09 -0400

    Erase unnecessary whitespaces, arrange code from lines in Part06 and improve code in recipe_search.py

--- Part06/part06-03_matrix/part06-03_matrix.py
+++ Part06/part06-03_matrix/part06-03_matrix.py
@@ -1,34 +1,37 @@
 # write your solution here
-	def matrix_sum():
-	    sum_by_row = row_sums()
-	    return sum(sum_by_row)
-	 
-	def matrix_max():
-	    max:int = 0
-	    with open("matrix.txt") as data:
-	        for row in data:
-	            row_elements = row.split(",")
-	            for ele in row_elements:
-	                if int(ele) > max:
-	                    max = int(ele)
-	    return max
-	 
-	def row_sums():
-	    sum:list = []
-	    with open("matrix.txt") as data:
-	        for row in data:
-	            single_row = row.split(",")            
-	            addition:int=0
-	            for n in single_row:            
-	                addition += int(n)
-	            sum.append(addition)
-            
-	    return sum
-	 
-	def matrix():
-	    print(f"The total sum of file data is: {matrix_sum()}")
-	    print(f"The maximun found value in the matrix is: {matrix_max()}")
-	 
-	if __name__=="__main__":
-	    matrix()
-	 
+def matrix_sum():
+	sum_by_row = row_sums()
+	return sum(sum_by_row)
+
+
+def matrix_max():
+	max_num: int = 0
+	with open("matrix.txt") as data:
+		for row in data:
+			row_elements = row.split(",")
+			for ele in row_elements:
+				if int(ele) > max_num:
+					max_num = int(ele)
+	return max_num
+
+
+def row_sums():
+	sum_num: list = []
+	with open("matrix.txt") as data:
+		for row in data:
+			single_row = row.split(",")
+			addition: int = 0
+			for n in single_row:
+				addition += int(n)
+			sum_num.append(addition)
+
+	return sum_num
+
+
+def matrix():
+	print(f"The total sum of file data is: {matrix_sum()}")
+	print(f"The maximun found value in the matrix is: {matrix_max()}")
+
+
+if __name__ == "__main__":
+	matrix()
